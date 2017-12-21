43462ab7f141af082a6b5b4cfdd251552550bab4
Miracle <741556866@qq.com>
2017-12-21 23:06:09 +0800

    '完成了jsonp请求推荐的功能以及可以请求到数据并添加到recommend组件中'

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,14 +1,59 @@
 <template>
-    <div>
-       recommend
+    <div class="recommoend">
+        <div class="recommoend-content">
+            <div class="slider-wrapper">
+            </div>
+            <div class="recommoend-list">
+                <h1 class="list-title">热门歌单推荐</h1>
+                <ul>
+                </ul>
+            </div>    
+        </div>
     </div>  
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+
+export default {
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend: function () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+
+    .recommoend
+      position: fixed
+      width: 100%
+      top: 88px
+      bottom: 0
+      .recommoend-content
+        height: 100%
+        overflow: hidden
+        .slider-wrapper
+          position: relative
+          width: 100%
+          overflow: hidden 
+        .recommoend-list
+          .list-title
+            height: 65px
+            line-height: 65px
+            text-align: center
+            font-size: $font-size-medium
+            color: $color-theme        
 </style>
 
 
