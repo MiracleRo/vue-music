1627ec3f069c4bf085b534649aa80a3bfedd092a
MiracleRo <741556866@qq.com>
2018-01-05 13:31:24 +0800

    将获取的歌手数据按顺序排列

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,7 +5,11 @@
 
 <script type="text/ecmascript-6">
 import {getSingerList} from 'api/singer'
+import Singer from 'common/js/singer.js'
 import {ERR_OK} from 'api/config'
+
+const HOT_NAME = '热门'
+const HOT_NAME_LEN = 10
 
 export default {
   data () {
@@ -20,9 +24,44 @@ export default {
     _getSingerList () {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
+          console.log(this._normalizeSinger(res.data.list))
         }
       })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_NAME_LEN) {
+          map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+      })
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
   }
 }
