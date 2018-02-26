d4e1b7d9db799e466bee01895e97cab5e221c94e
Miracle <741556866@qq.com>
2018-02-26 11:11:34 +0800

    完成歌手页面右侧导航栏 并且完成右侧导航栏与页面的联动

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -11,6 +11,13 @@
         </ul>
       </li>  
     </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart"> 
+      <ul>
+        <li v-for="item in shortcutList" class="item">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
   </scroll>
 </template>
 
@@ -22,6 +29,13 @@ export default {
     data: {
       type: Array,
       default: []
+    }
+  },
+  computed: {
+    shortcutList () {
+      return this.data.map((group) => {
+        return group.title.substr(0, 1)
+      })
     }
   },
   components: {
@@ -59,5 +73,41 @@ export default {
          .name
            margin-left: 20px
            color: $color-text-l
-           font-size: $font-size-medium     
+           font-size: $font-size-medium
+     .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+     .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)              
 </style>
